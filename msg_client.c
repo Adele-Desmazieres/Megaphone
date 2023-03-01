c86df659b339c9782d6fef91407a3f519e867832
matteo <matteobecherra+git@gmail.com>
2023-03-02 00:04:40 +0100

    fonctions struct <-> msg fix

--- msg_client.c
+++ msg_client.c
@@ -32,19 +32,22 @@ msg_client* msg_client_constr(int codereq, int id, int numfil, int nb, int datal
 uint16_t * msg_client_to_send(msg_client struc){
 
     //Taille: 6 octets si inscription sinon, dépend de la taille du texte
-    uint16_t * msg = (struc.is_inscript) ? (sizeof(uint16_t) * 6) : (sizeof(uint16_t) * (4 + (strlen(struc.data) - 1) /2) );
+    uint16_t * msg = (struc.is_inscript) ? malloc(sizeof(uint16_t) * 6) : malloc(sizeof(uint16_t) * (4 + (strlen(struc.data) - 1) /2) );
 
     //ENTETE
     //CODEREQ | ID
-    msg[0] = htons(struc.codereq) << 11 | htons(struc.id) ;
+    msg[0] = htons((struc.codereq << 11) + struc.id) ;
 
     //En cas d'inscription
-    if(!struc.is_inscript){
+    if(struc.is_inscript){
         //PSEUDO
+        //printf("Taille du pseudo = %d\n", strlen(struc.data));
         for (int i = 0; i < 10; i += 2) {
             //En cas de dépassement, on remplit de #
             char car1 = ( i < strlen(struc.data)) ? struc.data[i] : '#';
             char car2 = ( i+1 < strlen(struc.data)) ? struc.data[i+1] : '#';
+
+            //printf("%c %c \n", car1, car2);
 
             msg[(i/2) + 1] = htons(((int)car1  << 8) + (int)car2);
         }
@@ -52,30 +55,27 @@ uint16_t * msg_client_to_send(msg_client struc){
         return msg;
     }
 
-    printf("Numfil\n");
-
     //NUMFIL
     msg[1] = htons(struc.numfil);
-
-    printf("Nb\n");
 
     //NB
     msg[2] = htons(struc.nb);
 
-    printf("Data\n");
-
     //DATALEN | DATA[0]
     msg[3] = htons( (struc.datalen << 8) + (int)struc.data[0] );
-
-    printf("Data restant\n");
     
     //DATA restant
-    for (int i = 2; i < struc.datalen; i += 2) {
+    int data_pointer = 1;
+    for (int i = 4; data_pointer < strlen(struc.data); data_pointer+= 2, i++) {
         //En cas de dépassement on remplit par des #, comme pour le pseudo
-            char car1 = ( i < strlen(struc.data)) ? struc.data[i] : '#';
-            char car2 = ( i+1 < strlen(struc.data)) ? struc.data[i+1] : '#';
+            char car1 = ( data_pointer < strlen(struc.data)) ? struc.data[data_pointer] : '#';
+            char car2 = ( data_pointer+1 < strlen(struc.data)) ? struc.data[data_pointer+1] : '#';
 
-            msg[(i/2) + 4] = htons(((int)car1 << 8) + (int)car2);
+            //printf("%c %c \n", car1, car2);
+            msg[i] = htons(((int)car1 << 8) + (int)car2);
+
+            //printf("%d \n", msg[i]);
+            
     }
     return msg; 
 }
@@ -83,15 +83,14 @@ uint16_t * msg_client_to_send(msg_client struc){
 //Elimine les # en fin de chaine, alloue la valeur de retour
 char * get_real_name(const char * placeholder){
     int i = 0;
-    for(; *placeholder != '\0' && *placeholder != '#'; placeholder++; i++){}
+    for(; *placeholder != '\0' && *placeholder != '#'; placeholder++, i++){}
     char * ret = malloc(sizeof(char) * (i+1));
-    strncat(ret, placeholder, i);
+    strncat(ret, placeholder-i, i);
 
     return ret;
 }
 
 msg_client * tcp_to_msgclient(uint16_t * msg) {
-    printf("Debut\n");
 
     //ENTETE
     //CODEREQ | ID
@@ -100,13 +99,18 @@ msg_client * tcp_to_msgclient(uint16_t * msg) {
     int id = entete - (codereq << 11);
 
     if( codereq == 1 ) {
+        //printf("ICI\n");
         char * pseudo = malloc(11 * sizeof(char));
-        for(int i = 1; i < 6; i++){
+        int pseudo_pointer = 0;
+        for(int i = 1; pseudo_pointer < 11; pseudo_pointer += 2, i++){
             uint16_t cars = ntohs(msg[i]);
             char car1 = (char)(cars >> 8);
             char car2 = (char)(cars - (car1 << 8));
-            pseudo[((i-1)*2)] = car1;
-            pseudo[((i-1)*2)+1] = car2;
+
+            //printf("%c %c\n", car1, car2);
+
+            pseudo[pseudo_pointer] = car1;
+            pseudo[pseudo_pointer+1] = car2;
         }
         pseudo[10] = '\0';
 
@@ -127,26 +131,33 @@ msg_client * tcp_to_msgclient(uint16_t * msg) {
     char car1 = (char)(datalenData - (datalen << 8));
 
     //Si datalen = 0, erreur...
-    if(datalen == 0) { perror("Null data, exiting\n"); exit(1); }
+    if(datalen <= 0) { perror("Null data, exiting\n"); exit(1); }
 
     //On alloue une chaine de taille datalen+1
     char * finalData = malloc((datalen+1) * sizeof(char) );
     //On ajoute le premier caractère
     finalData[0] = car1;
+    //printf("%c \n", finalData[0]);
+    int data_pointer = 1;
 
-        for(int i = 1; i < datalen; i++){
+        for(int i = 4; data_pointer < datalen ; data_pointer += 2, i++){
 
-            uint16_t cars = ntohs(msg[i/2]);
+            uint16_t cars = ntohs(msg[i]);
+            //printf("%d \n", msg[i]);
             
             //On prends les deux caractères représentés par l'octet...
-            char car1 = (i < datalen) ? (char)(cars >> 8) : '\0';
-            char car2 = (i+1 < datalen) ? (char)(cars - (car1 << 8)) : '\0';
+            uint16_t car1_int = (cars >> 8); 
+            char car1 = (char)car1_int;
+            char car2 = (data_pointer+1 < datalen) ? (char)(cars - (car1_int << 8)) : '\0';
+
+            //printf("%c %c\n", car1, car2);
 
             //... or si i ou i+1 dépasse datalen, c'est qu'on a fini
-            finalData[i] = car1;
+            finalData[data_pointer] = car1;
             if(car1 == '\0') break;
-            finalData[i+1] = car2;
+            finalData[data_pointer+1] = car2;
             if(car2 == '\0') break;
+
         }
 
     return msg_client_constr(codereq, id, numfil, nb, datalen, finalData, 0);
