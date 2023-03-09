c2fefabb34a9f448b806b3f8806d482d1fc6b4c3
BECHERRA matteo <matteobecherra@gmail.com>
2023-03-09 18:21:56 +0100

    Debut de la concurrence pour le serveur, pas encore fonctionnel

--- serveur.c
+++ serveur.c
@@ -7,10 +7,31 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <stdbool.h>
+#include <pthread.h>
 
 #include "serveur.h"
+#include "msg_client.h"
+#include "msg_serveur.h"
+#include "bdd_serveur.h"
 
 #define SIZE_MSG 512
+
+typedef struct objets_a_passer_au_thread_dans_une_structure {
+    user_list * liste_utilisateurs;
+    liste_fils * liste_fils;
+    int * socketclient;
+} objets_a_passer_au_thread_dans_une_structure;
+
+objets_a_passer_au_thread_dans_une_structure * objet_thread_constr(user_list * ul, liste_fils * lf, int * sockclient){
+    objets_a_passer_au_thread_dans_une_structure * ret = malloc(sizeof(objets_a_passer_au_thread_dans_une_structure));
+    if (ret == NULL) perror("Erreur malloc objets thread structure\n");
+
+    ret->liste_utilisateurs = ul;
+    ret->liste_fils = lf;
+    ret->socketclient = sockclient;
+
+    return ret;
+}
 
 int main(int argc, char **argv) {
     return creation_serveur();
@@ -54,49 +75,83 @@ int creation_serveur() {
 */
 int accepter_clients(int sock) {
     //On écoute chaque clients
-    int sockclient = accept(sock, NULL, NULL);
-    if (sockclient == -1) {
-        perror("Probleme socket client\n");
-        close(sock);
-        return 1;
+
+    int serveur_running = 1;
+    
+    user_list * liste_users = user_list_constr();
+    liste_fils * liste_fil = liste_fils_constr();
+
+    //Boucle principale
+    while(serveur_running){
+
+        int sockclient = accept(sock, NULL, NULL);
+        if (sockclient == -1) {
+            perror("Probleme socket client\n");
+            close(sock);
+            continue;
+        }
+        int * sockcli_malloc = malloc(sizeof(int));
+        *sockcli_malloc = sockclient;
+
+        objets_a_passer_au_thread_dans_une_structure * struc = objet_thread_constr(liste_users, liste_fil, sockcli_malloc);
+
+        //Création du thread et lancement de sa routine
+        pthread_t thread;
+        if(pthread_create(&thread, NULL, communication_client, struc) == -1){
+            perror("Echec connexion\n");
+            continue;
+        }
+
+
+        //ca peut ptet poser des problemes ici, faudrait attendre que le thread soit termine pour les free
+        free(sockcli_malloc);
+        free(struc);
+
     }
 
-    //Creation de la structure pour stocker l'adresse client.
-    struct sockaddr_in adrclient;
-    memset(&adrclient, 0, sizeof(adrclient));
-    socklen_t size = sizeof(adrclient);
-
-    //Affichage d'un message avec l'adresse client.
-    char addr_buf[INET_ADDRSTRLEN];
-    memset(addr_buf, 0, sizeof(addr_buf));
-    inet_ntop(AF_INET6, &(adrclient.sin_addr), addr_buf, sizeof(addr_buf));
-    printf("Client connecte : %s %d\n", addr_buf, ntohs(adrclient.sin_port));
-
-    int retour = communication_client(sockclient);
-    close(sock);
-
-    return retour;
-}
-
-/*
-    Effectue la boucle de communication entre le serveur et le/les clients.
-*/
-int communication_client(int sockclient) {
-    char BUF[512];
-    memset(BUF, '\0', 512);
-
-    //Recevoir/Envoyer les messages.
-
-    close(sockclient);
+    free_liste_fils(liste_fil);
+    free_userlist(liste_users);
 
     return 0;
 }
 
 /*
-    Renvoie une instance de msg_client en fonction de codereq.
+    Effectue la communication entre le serveur et le/les clients.
 */
-msg_client reponse_tcp(char * buf) {
-    msg_client reponse;
-    
-    return reponse;
+void * communication_client(void * arg) {
+
+    objets_a_passer_au_thread_dans_une_structure * obj = ((objets_a_passer_au_thread_dans_une_structure *) arg);
+    int sockcli = *(obj->socketclient); 
+    char BUF[512];
+    memset(BUF, '\0', 512);
+
+    int recu = recv(sockcli, BUF, 512, 0);
+
+    if(recu <= 0){
+        close(sockcli);
+        return NULL;
+    }
+
+    msg_client * msg_recu_traduit = tcp_to_msgclient((uint16_t *)BUF);
+
+    switch(msg_recu_traduit->codereq){
+
+        //... TODO TO DO : traiter les autres demandes
+
+        default :
+
+            msg_serveur * to_send = msg_serveur_constr(31,0,0,0);
+            uint16_t * msg_reponse = msg_serveur_to_send(*to_send);
+            int snd = send(sockcli, msg_reponse, 512, 0);
+            if (snd <= 0){
+                perror("Erreur envoi réponse\n");
+                return NULL;
+            }
+
+            free(to_send); free(msg_reponse);
+
+    } 
+
+    close(sockcli);
+    return NULL;    
 }
--- serveur.h
+++ serveur.h
@@ -6,8 +6,7 @@ int creation_serveur();
 //Accepte les clients avec en parametre la socket serveur.
 int accepter_clients(int sockserv);
 
-//Effectue la boucle de communication entre le serveur et le/les clients.
-int communication_client(int sock_client);
-
 //Renvoie une instance de msg_client en fonction de codereq.
 msg_client reponse_tcp(char * buf);
+
+void * communication_client(void * arg);
