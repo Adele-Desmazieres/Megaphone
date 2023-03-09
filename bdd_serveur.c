e174075f5cbe1c8990bd6920543f3957485a4218
BECHERRA matteo <matteobecherra@gmail.com>
2023-03-09 10:52:13 +0100

    quick-fix dernier commit

--- bdd_serveur.c
+++ bdd_serveur.c
@@ -1,4 +1,9 @@
 
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
+
 #include "bdd_serveur.h"
 
 /*
--- bdd_serveur.h
+++ bdd_serveur.h
@@ -1,10 +1,4 @@
 
-#include <stdlib.h>
-#include <string.h>
-#include <stddef.h>
-#include <stdio.h>
-
-
 //STRUCTURE BILLET
 typedef struct billet billet;
 typedef struct billet {
