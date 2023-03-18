a2e2a3d72e5e78d1754031ee54d33d3242d3b005
Delta-Chat <adele.desmazi@gmail.com>
2023-03-18 17:33:00 +0100

    Inscription

--- interpreteur.c
+++ interpreteur.c
@@ -2,8 +2,83 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "client.h"
+#include "msg_client.h"
+#include "msg_serveur.h"
+
 
 #define MSG_LIMIT 255
+
+
+
+int main() {
+    
+    
+    
+    return 0;
+}
+
+
+/* 
+    Inscrit l'utilisateur : 
+    - en cas de réussite, initialise userid et renvoie 0
+    - sinon renvoie 1
+*/ 
+int inscription(int *userid) {
+    char str_input[10];
+    printf("Entrez votre pseudo > ");
+    fgets(str_input, 10, stdin); // 10 = taille max du pseudo, imposé par le sujet
+    int n = strlen(str_input);
+    // quitte l'inscription si rien n'est entré
+    if (n == 0) {
+        return 1;
+    }
+    // redemande un pseudo s'il est trop long
+    while (n > 10) { // TODO : vérifier que le pseudo contient que des caractères alphanum ?
+        printf("Votre pseudo peut contenir au plus 10 caractères.\n");
+        printf("Entrez votre pseudo > ");
+        fgets(str_input, 10, stdin);
+        int n = strlen(str_input);
+        if (n == 0) {
+            return 1;
+        }
+    }
+    
+    // envoie le message contenant le pseudo en suivant le protocole
+    msg_client *mstruct = msg_client_constr(1, 0, -1, -1, n, str_input, 1);
+    u_int16_t *marray = msg_client_to_send(*mstruct);
+    int sock = connexion_6();
+    int size_exchanged = send(sock, marray, 6, 0); // TODO est-ce bien ca qu'il faut envoyer ? taille 6 ?
+    if (size_exchanged != 6) goto error;
+    
+    // recoit l'identifiant en réponse
+    u_int16_t buff[3];
+    size_exchanged = recv(sock, buff, 3, 0);
+    if (size_exchanged != 3) goto error;
+    
+    // interprète la réponse
+    msg_serveur *rep = tcp_to_msgserveur(buff);
+    if (rep->codereq != 1) { // échec du côté serveur
+        printf("Echec de l'inscription côté serveur.");
+        return 1;
+    }
+    
+    // réussite
+    *userid = rep->id;
+    close(sock);
+    return 0;
+
+    error:
+    perror("Erreur connexion au serveur.\n"); 
+    close(sock);
+    return EXIT_FAILURE;
+
+}
+
+
 
 
 /*
@@ -11,7 +86,7 @@
 */
 int poster_billet() {
     char str_input[MSG_LIMIT];
-    printf("Entrez votre message > \n");
+    printf("Entrez votre message > ");
     fgets(str_input, MSG_LIMIT, stdin);
     
     // TODO transformer la string en struct puis en tableau de données
@@ -19,8 +94,6 @@ int poster_billet() {
     
     return 0;
 }
-
-
 
 /*
     Interpreteur du côté utilisateur
@@ -37,7 +110,7 @@ int interpreteur_utilisateur() {
         fgets(str_input, MSG_LIMIT, stdin);
         
         // TODO vérifier que le charactère d'input est un entier
-        // compris entre 0 et le nombre de valeurs possibles
+        // compris entre 1 et le nombre de valeurs possibles
         
         int nbr_input = atoi(str_input);
         
