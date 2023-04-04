b7262de5bbb907953efb6e955b38e5d4b66fb7dd
Delta-Chat <adele.desmazi@gmail.com>
2023-04-04 20:17:22 +0200

    POSTER DES MESSAGES PAR L'UTILISATEUR

--- ClientServeur/interpreteur.c
+++ ClientServeur/interpreteur.c
@@ -121,12 +121,13 @@ int inscription(int *userid) {
         }
     }
     
-    // envoie le message contenant le pseudo en suivant le protocole
+    // crée le message contenant le pseudo en suivant le protocole
     msg_client *mstruct = msg_client_constr(1, 0, -1, -1, n, str_input, 1);
     
     //printf("PseudoClient : %s\n", mstruct->data);
     u_int16_t *marray = msg_client_to_send(*mstruct);
 
+    // envoie le message
     int sock = connexion_6();
     if (sock == -1) return EXIT_FAILURE;
     int size_exchanged = send(sock, marray, 12, 0); 
@@ -168,7 +169,7 @@ int debut_session(int *userid) {
     fgets(str_input, 10, stdin); // 10 = taille max du pseudo, imposé par le sujet
     int n = strlen(str_input);
     // quitte la fonction si rien n'est entré
-    if (n == 0) {
+    if (n <= 1) {
         return 1;
     }
     // redemande un identifiant s'il est trop long
@@ -176,7 +177,7 @@ int debut_session(int *userid) {
         printf("Votre identifiant doit être un nombre entier > ");
         fgets(str_input, ID_LIMIT, stdin);
         int n = strlen(str_input);
-        if (n == 0) {
+        if (n <= 1) {
             return 1;
         }
     }
@@ -197,9 +198,9 @@ int interpreteur_utilisateur(int *userid) {
     int session_continue = 1;
 
     while (session_continue) {
-        
+        printf("\n");
         lister_commandes_en_session();
-        printf(" Entrez 7 pour afficher les différentes commandes.\nQue voulez-vous faire ? > ");
+        printf("Entrez 7 pour afficher les différentes commandes.\nQue voulez-vous faire ? > ");
         fgets(str_input, MSG_LIMIT, stdin);
 
         if (!isdigit(str_input[0]) || atoi(str_input) > 7 || atoi(str_input) < 0) {
@@ -208,37 +209,36 @@ int interpreteur_utilisateur(int *userid) {
 
         else {
             int nbr_input = atoi(str_input);
-            
             switch (nbr_input) {
             
                 case 1: // mettre fin à la session
                     session_continue = 0;
                     break;
-                    
+                
                 case 2: // poster un billet
                     poster_billet_client(userid);
                     break;
-                    
+                
                 case 3: // demander la liste des n derniers billets
                 
                     break;
-                    
+                
                 case 4: // s'abonner à un fil
                 
                     break;
-                    
+                
                 case 5: // poster un fichier
                 
                     break;
-                    
+                
                 case 6: // telecharger un fichier
                 
                     break;
-                    
+                
                 case 7: // lister des commandes
                     lister_commandes_en_session();
                     break;
-                    
+                
                 default:
                     printf("Réponse non reconnue.\n");
                     break;
@@ -249,22 +249,9 @@ int interpreteur_utilisateur(int *userid) {
     return 0;
 }
 
-
 /*
-    Crée un billet et l'envoie au serveur
+    Affiche les commandes accessibles lorsqu'un utilisateur est en cours de session. 
 */
-int poster_billet_client(int *userid) {
-    char str_input[MSG_LIMIT];
-    printf("Entrez votre message > ");
-    fgets(str_input, MSG_LIMIT, stdin);
-    
-    // TODO transformer la string en struct puis en tableau de données
-    // puis l'envoyer au serveur
-    
-    return 0;
-}
-
-
 int lister_commandes_en_session() {
     printf("1 : fin de session\n");
     printf("2 : poster un message\n");
@@ -275,3 +262,111 @@ int lister_commandes_en_session() {
     printf("7 : affichage des commandes\n");
     return 0;
 }
+
+
+/*
+    Vérifie qu'un string ne contient que des nombres, et au moins un charactère. 
+*/
+int string_is_number(char *s) {
+    if (s == NULL) return 0;
+    for (int i = 0; i < strlen(s); i++) {
+        if (! isdigit(s[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// https://stackoverflow.com/questions/8164000/how-to-dynamically-allocate-memory-space-for-a-string-and-get-that-string-from-u
+// TODO : à remplacer par readline
+char *getln() {
+    char *line = NULL; 
+    char *tmp = NULL;
+    size_t size = 0;
+    size_t index = 0;
+    int c = EOF;
+
+    while (c) {
+        c = getc(stdin);
+
+        // Check if we need to stop, and replace the ending-char by the \0
+        if (c == EOF || c == '\n') {
+            c = 0;
+        }
+
+        // Check if we need to expand
+        if (size <= index) {
+            size += MSG_LIMIT;
+            tmp = realloc(line, size);
+            if (!tmp) {
+                free(line);
+                line = NULL;
+                break;
+            }
+            line = tmp;
+        }
+
+        // Actually store the character 
+        line[index++] = c;
+    }
+    return line;
+}
+
+
+/*
+    Crée un billet et l'envoie au serveur
+*/
+int poster_billet_client(int *userid) {
+    char *str_input;
+    char *num_input;
+    int n;
+    
+    while (! string_is_number(num_input)) { 
+        printf("Entrez le numéro du fil sur lequel envoyer le message > ");
+        num_input = getln();
+        n = strlen(num_input);
+        if (n <= 0) {
+            return 1;
+        }
+    }
+    
+    printf("Entrez votre message > ");
+    str_input = getln();
+    
+    int numfil = atoi(str_input);
+    n = strlen(str_input);
+    
+    // crée le message
+    msg_client *mstruct = msg_client_constr(2, *userid, numfil, 0, n, str_input, 0);
+    //printf("Message : %s\n", mstruct->data);
+    u_int16_t *marray = msg_client_to_send(*mstruct);
+    
+    // l'envoie
+    int sock = connexion_6();
+    if (sock == -1) return EXIT_FAILURE;
+    int size_exchanged = send(sock, marray, 12, 0); 
+    if (size_exchanged != 12) goto error;
+    
+    // recoit la réponse
+    u_int16_t buff[3];
+    size_exchanged = recv(sock, buff, 6, 0);
+    if (size_exchanged != 6) goto error;
+    
+    // interprète la réponse
+    msg_serveur *rep = tcp_to_msgserveur(buff);
+    if (rep->codereq != 2) { // échec du côté serveur
+        printf("Echec du traitement côté serveur.\n");
+        return 1;
+    }
+    
+    // réussite
+    printf("1 message envoyé sur le fil %d.\n", rep->numfil);
+    close(sock);
+    return 0;
+
+    error:
+    printf("Erreur communication avec le serveur.\n");
+    close(sock);
+    return EXIT_FAILURE;
+}
+
