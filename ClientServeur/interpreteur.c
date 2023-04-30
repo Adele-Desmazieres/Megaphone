f6a4d42b75c43ab6168cce5648c34800017a9a44
BECHERRA matteo <matteobecherra@gmail.com>
2023-04-30 16:02:22 +0200

    toujours problème de réception multiple pour le multicast

--- ClientServeur/interpreteur.c
+++ ClientServeur/interpreteur.c
@@ -24,7 +24,6 @@
 
 pthread_mutex_t verrou = PTHREAD_MUTEX_INITIALIZER;
 
-
 struct pollfd * notrepoll;
 int * tailledepoll;
 
@@ -661,17 +660,22 @@ int abonnement_fil(int userid){
     poll_specifique.fd = sock_multicast;
     poll_specifique.events = POLLIN;
 
-    printf("Hello2\n");
+    printf("Socket ouverte pour le multicast? %d\n", sock_multicast);
     pthread_mutex_lock(&verrou);
 
-    notrepoll = realloc(notrepoll, sizeof(struct pollfd) * ( (*tailledepoll) + 1) );
+    notrepoll = realloc(notrepoll, sizeof(struct pollfd) * ((*tailledepoll) + 1) );
     if (notrepoll == NULL){
         perror("Echec realloc @ abonnement_fil\n");
     }
+    //notrepoll[*tailledepoll] = poll_specifique;
     memcpy(notrepoll + (*tailledepoll), &poll_specifique, sizeof(struct pollfd));
     int nouv_taille_poll = (*tailledepoll + 1);
     memcpy(tailledepoll, &nouv_taille_poll, sizeof(int));
+    printf("Socket ouverte pour le multicast, dans la var globale? %d\n", (notrepoll[(*tailledepoll) - 1]).fd);
     pthread_mutex_unlock(&verrou);
+    printf("tailledepol = %d\n", *tailledepoll);
+    printf("pointeur vers structure dans abonnement : %p\n", (void *)notrepoll);
+
 
     printf("Abonné avec succès au fil %d\n", infos_ip->numfil);
 
@@ -685,23 +689,39 @@ void * thread_notifs(void * args){
 
     while(1){
 
+
         pthread_mutex_lock(&verrou);
         if(*tailledepoll == 0){ pthread_mutex_unlock(&verrou); continue; }
         pthread_mutex_unlock(&verrou);
 
-        if(poll(notrepoll, *tailledepoll, 5000) == 0) { /*pthread_mutex_unlock(&verrou);*/ continue; }
-            for(int i = 0; i < *tailledepoll; i++ ){
+        sleep(5);
+        int compteur = 0;
+        pthread_mutex_lock(&verrou);
+
+
+
+        if( (compteur = poll(notrepoll, *tailledepoll, 5000)) == 0) { pthread_mutex_unlock(&verrou); continue; }
+
+            printf("Socket ouverte pour le multicast, dans la var globale dans le thread? %d\n", (notrepoll[*tailledepoll]).fd);
+            printf("pointeur vers structure dans thread : %p\n", (void *)notrepoll);
+            //pthread_mutex_lock(&verrou);
+            for(int i = 0; i < (*tailledepoll); i++, compteur-- ){
             printf("Hello : %d : %d\n", *tailledepoll, i);
 
-                struct pollfd actuel = {0};
-                pthread_mutex_lock(&verrou);
-                actuel = notrepoll[i];
+            if(compteur <= 0) break;
+            printf("compteur de poll : %d\n", compteur);
+
+                struct pollfd actuel;
+                memcpy(&actuel, notrepoll+i, sizeof(struct pollfd));
                 printf("SOCK ACTUEL = %d\n", actuel.fd);
+                printf("PRET EN LECTURE???? : %d\n", (actuel.revents));
+
+                if ((actuel.revents & POLLIN) == 0) continue;
                 if (actuel.revents & POLLIN){
                     //Alors ce descripteur est prêt pour la lecture;
 
                     u_int16_t buf[SIZE_MSG_NOTIF] = {0};
-                    if (read(actuel.fd, buf, SIZE_MSG_NOTIF) < 0){
+                    if (read(actuel.fd, buf, SIZE_MSG_NOTIF) <= 0){
                         perror("Read sur descripteur de notif\n");
                     }
 
@@ -714,8 +734,8 @@ void * thread_notifs(void * args){
                     free(msg_a_afficher->data);
                     free(msg_a_afficher);                    
                 }
-                pthread_mutex_unlock(&verrou);
             }
+            pthread_mutex_unlock(&verrou);
 
     }
 
--- ClientServeur/serveur.c
+++ ClientServeur/serveur.c
@@ -282,7 +282,7 @@ int poster_billet(msg_client * msg_client, liste_fils * liste_fils, user_list * 
 
         //Pour le multicast, on envoie une notif si le fil est en abonnement
         if (fil_poster->is_multicast){
-            msg_notif to_snd = { .codereq = 4, .id = 0, .numfil = fil_poster->id, .pseudo = username, .data = contenu};
+            msg_notif to_snd = { .codereq = 4, .id = 0, .numfil = fil_poster->id+1, .pseudo = username, .data = contenu};
             u_int16_t * buf = msg_notif_to_udp(to_snd);
 
             printf("SOCK OU ON ENVOIE : %d\n", fil_poster->multicast_sockfd);
